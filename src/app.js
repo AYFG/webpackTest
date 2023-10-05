9d6507bcc67b74c6f4ff57fac0dec934f028fa27
AYFG <sek82468246@gmail.com>
2023-10-05 15:53:55 +0900

    이미지 최적화 플러그인 등 추가했습니다

--- src/app.js
+++ src/app.js
@@ -4,7 +4,20 @@
 // console.log(plus(2, 3));
 import "./style.css";
 import rabbit from "./rabbit.png";
+import bg from "./background.png";
+// console.log(pw);
+
+let env;
+
+// module.exports의 mode가 development면 dev , production 이면 pro
+if (process.env.NODE_ENV === "development") {
+  env = dev;
+} else {
+  env = pro;
+}
+
+console.log(env);
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.innerHTML = `<img src="${rabbit}"/>`;
+  document.body.innerHTML = `<img src="${bg}"/>`;
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require("path");
 const webpack = require("webpack");
 const childProcess = require("child_process");
+require("dotenv").config();
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 // 모듈을 밖으로 빼내는 노드 JS문법이다. 엔트리 , 아웃풋 그리고 번들링 모드를 설정할 수 있다.
 
+// mode가 production이면 main.js가 난독화되어 해석이 안된다.
 module.exports = {
-  mode: "development",
+  mode: process.env.NODE_ENV === "development" ? "development" : "production",
   entry: {
     main: path.resolve("./src/app.js"),
   },
@@ -44,5 +49,31 @@ module.exports = {
       마지막 빌드 시간은 : ${new Date().toLocaleString()}
       `,
     }),
+    new webpack.DefinePlugin({
+      // pw: 123456,
+      // dev: JSON.stringify("https://dev.api.com"),
+      // pro: JSON.stringify("https://pro.api.com"),
+      dev: JSON.stringify(process.env.DEV_API),
+      pro: JSON.stringify(process.env.PRO_API),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new CleanWebpackPlugin(),
   ],
+  optimization: {
+    // 이미지 압축 작업을 실행할지 결정함.
+    minimize: true,
+    minimizer: [
+      new ImageMinimizerPlugin({
+        test: /\.(jpe?g|png|gif|svg)/i,
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [["imagemin-optipng", { optimizationLevel: 3 }]],
+          },
+        },
+      }),
+    ],
+  },
 };
